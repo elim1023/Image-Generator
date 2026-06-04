46cb496b3759bd31d3ac339bcbae9098fa769ce6
geogeolo <163122629+geogeolo@users.noreply.github.com>
2026-06-05 04:33:46 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,18 +2,17 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
-# Loading environment variables from a .env file
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def generateImg(text, sz):
-    # Creating an image using given prompt text and image size
     img = client.images.generate(
-        model="dall-e-3",
+        model="gpt-image-1",
         prompt=text,
         n=1,
-        size=sz
+        size=sz,
+        quality="medium"
     )
 
     return img.data[0].url
